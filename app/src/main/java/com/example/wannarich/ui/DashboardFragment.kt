5fe5c519c2bd945eef825baf45be7d0874313bda
wenlong.teo <wenlong.teo@qumonintelligence.com>
2021-01-30 21:12:22 +0800

    rename DashboardFragment.kt

--- app/src/main/java/com/example/wannarich/ui/DashboardFragment.kt
+++ app/src/main/java/com/example/wannarich/ui/DashboardFragment.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import com.example.wannarich.R
-import com.example.wannarich.databinding.FragmentTransactionBinding
+import com.example.wannarich.databinding.FragmentDashboardBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class TransactionFragment : Fragment(R.layout.fragment_transaction) {
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
 
-    private lateinit var binding: FragmentTransactionBinding
+    private lateinit var binding: FragmentDashboardBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding = FragmentTransactionBinding.bind(view)
+        binding = FragmentDashboardBinding.bind(view)
 
 
     }
